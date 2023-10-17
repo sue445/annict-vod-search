705a496ade450c7c1f51ce213d9d252c46b057e2
sue445 <sue445@sue445.net>
2023-10-17 23:09:04 +0900

    Add buttons

--- docs/src/app.rb
+++ docs/src/app.rb
@@ -18,9 +18,12 @@ view = ->(state, actions) {
         />
       </div>
 
-      <div class="btn-group">
-        <a class="btn btn-primary" role="button" target="_blank" href="https://animestore.docomo.ne.jp/animestore/sch_pc?searchKey=#{state[:search_word]}&vodTypeList=svod_tvod">dアニメストアで検索する<i class="bi bi-box-arrow-up-right"></i></a>
-      </div>
+      <a class="btn btn-info" role="button" target="_blank" href="https://www.b-ch.com/search/text/?search_txt=#{state[:search_word]}">バンダイチャンネルで検索する<i class="bi bi-box-arrow-up-right"></i></a>
+      <a class="btn btn-dark" role="button" target="_blank" href="https://ch.nicovideo.jp/search/#{state[:search_word]}?type=channel&mode=s&sort=c&order=d">ニコニコチャンネルで検索する<i class="bi bi-box-arrow-up-right"></i></a>
+      <a class="btn btn-primary" role="button" target="_blank" href="https://animestore.docomo.ne.jp/animestore/sch_pc?vodTypeList=svod_tvod&searchKey=#{state[:search_word]}">dアニメストアで検索する<i class="bi bi-box-arrow-up-right"></i></a>
+      <a class="btn btn-warning" role="button" target="_blank" href="https://www.amazon.co.jp/s?i=instant-video&k=#{state[:search_word]}">Amazon プライム・ビデオで検索する<i class="bi bi-box-arrow-up-right"></i></a>
+      <a class="btn btn-danger" role="button" target="_blank" href="https://www.google.com/search?q=site%3Awww.netflix.com+#{state[:search_word]}">Netflixで検索する<i class="bi bi-box-arrow-up-right"></i></a>
+      <a class="btn btn-success" role="button" target="_blank" href="https://abema.tv/search?q=#{state[:search_word]}">Abemaで検索する<i class="bi bi-box-arrow-up-right"></i></a>
     </div>
   HTML
 }
