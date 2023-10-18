95fe74b6dfa84cd813b85630f78347e07838b5ea
sue445 <sue445@sue445.net>
2023-10-19 00:30:23 +0900

    Fix XSS

--- docs/src/app.rb
+++ docs/src/app.rb
@@ -1,3 +1,5 @@
+require "cgi"
+
 state = {
   search_word: "",
   encoded_search_word: "",
@@ -8,6 +10,7 @@ actions = {
     value = value.strip
     state[:search_word] = value
     state[:encoded_search_word] = encode_uri(value)
+    state[:escaped_search_word] = CGI.escapeHTML(value)
   },
 }
 
@@ -40,12 +43,12 @@ view = ->(state, actions) {
         />
       </div>
       <div class="mb-3">
-        <a role="button" target="_blank" rel="noopener" class="btn btn-info"    href="https://www.b-ch.com/search/text/?search_txt=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>バンダイチャンネルで「#{state[:search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-dark"    href="https://ch.nicovideo.jp/search/#{state[:encoded_search_word]}?type=channel&mode=s&sort=c&order=d"><i class="bi bi-search"></i>ニコニコチャンネルで「#{state[:search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-primary" href="https://animestore.docomo.ne.jp/animestore/sch_pc?vodTypeList=svod_tvod&searchKey=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>dアニメストアで「#{state[:search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-warning" href="https://www.amazon.co.jp/s?i=instant-video&k=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Amazon プライム・ビデオで「#{state[:search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-danger"  href="https://www.google.com/search?q=site%3Awww.netflix.com+#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Netflixで「#{state[:search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-success" href="https://abema.tv/search?q=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Abemaで「#{state[:search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-info"    href="https://www.b-ch.com/search/text/?search_txt=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>バンダイチャンネルで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-dark"    href="https://ch.nicovideo.jp/search/#{state[:encoded_search_word]}?type=channel&mode=s&sort=c&order=d"><i class="bi bi-search"></i>ニコニコチャンネルで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-primary" href="https://animestore.docomo.ne.jp/animestore/sch_pc?vodTypeList=svod_tvod&searchKey=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>dアニメストアで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-warning" href="https://www.amazon.co.jp/s?i=instant-video&k=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Amazon プライム・ビデオで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-danger"  href="https://www.google.com/search?q=site%3Awww.netflix.com+#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Netflixで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-success" href="https://abema.tv/search?q=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Abemaで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
       </div>
     </div>
   HTML
