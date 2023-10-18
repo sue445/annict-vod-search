d454948c25f61832e0187057c4e91349d7772cb3
sue445 <sue445@sue445.net>
2023-10-19 00:36:36 +0900

    rename state

--- docs/src/app.rb
+++ docs/src/app.rb
@@ -2,16 +2,16 @@ require "cgi"
 
 state = {
   search_word: "",
-  encoded_search_word: "",
-  escaped_search_word: "",
+  uri_encoded_word: "",
+  html_encoded_word: "",
 }
 
 actions = {
   update_search_word: -> (state, value) {
     value = value.strip
     state[:search_word] = value
-    state[:encoded_search_word] = CGI.escape(value)
-    state[:escaped_search_word] = CGI.escapeHTML(value)
+    state[:uri_encoded_word] = CGI.escape(value)
+    state[:html_encoded_word] = CGI.escapeHTML(value)
   },
 }
 
@@ -26,12 +26,12 @@ view = ->(state, actions) {
         />
       </div>
       <div class="mb-3">
-        <a role="button" target="_blank" rel="noopener" class="btn btn-info"    href="https://www.b-ch.com/search/text/?search_txt=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>バンダイチャンネルで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-dark"    href="https://ch.nicovideo.jp/search/#{state[:encoded_search_word]}?type=channel&mode=s&sort=c&order=d"><i class="bi bi-search"></i>ニコニコチャンネルで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-primary" href="https://animestore.docomo.ne.jp/animestore/sch_pc?vodTypeList=svod_tvod&searchKey=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>dアニメストアで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-warning" href="https://www.amazon.co.jp/s?i=instant-video&k=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Amazon プライム・ビデオで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-danger"  href="https://www.google.com/search?q=site%3Awww.netflix.com+#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Netflixで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
-        <a role="button" target="_blank" rel="noopener" class="btn btn-success" href="https://abema.tv/search?q=#{state[:encoded_search_word]}"><i class="bi bi-search"></i>Abemaで「#{state[:escaped_search_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-info"    href="https://www.b-ch.com/search/text/?search_txt=#{state[:uri_encoded_word]}"><i class="bi bi-search"></i>バンダイチャンネルで「#{state[:html_encoded_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-dark"    href="https://ch.nicovideo.jp/search/#{state[:uri_encoded_word]}?type=channel&mode=s&sort=c&order=d"><i class="bi bi-search"></i>ニコニコチャンネルで「#{state[:html_encoded_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-primary" href="https://animestore.docomo.ne.jp/animestore/sch_pc?vodTypeList=svod_tvod&searchKey=#{state[:uri_encoded_word]}"><i class="bi bi-search"></i>dアニメストアで「#{state[:html_encoded_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-warning" href="https://www.amazon.co.jp/s?i=instant-video&k=#{state[:uri_encoded_word]}"><i class="bi bi-search"></i>Amazon プライム・ビデオで「#{state[:html_encoded_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-danger"  href="https://www.google.com/search?q=site%3Awww.netflix.com+#{state[:uri_encoded_word]}"><i class="bi bi-search"></i>Netflixで「#{state[:html_encoded_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
+        <a role="button" target="_blank" rel="noopener" class="btn btn-success" href="https://abema.tv/search?q=#{state[:uri_encoded_word]}"><i class="bi bi-search"></i>Abemaで「#{state[:html_encoded_word]}」を検索する<i class="bi bi-box-arrow-up-right"></i></a>
       </div>
     </div>
   HTML
